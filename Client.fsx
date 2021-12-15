305285b8cc2c00d3c7595fcbce00de306fb48106
Parth Gupta <gupta26parth@gmail.com>
2021-12-15 15:55:41 -0500

    Add files via upload

---
+++ Client.fsx
@@ -0,0 +1,213 @@
+﻿#r "nuget: Akka" 
+#r "nuget: Akka.FSharp" 
+#r "nuget: Akka.Remote" 
+#r "nuget: Akka.TestKit" 
+
+open Akka.Actor
+open Akka.Configuration
+open Akka.FSharp
+
+let args = fsi.CommandLineArgs
+let numberOfUsers = args.[1] |> int
+
+[<Struct>]
+type guid = {
+    id : string
+}
+type registerMsg = {
+    code : int; 
+    userId : guid; 
+    password : string
+}
+type tweetMsg = {
+    userId : guid; 
+    tweet : string
+}
+type subscribeMsg = {
+    userId : guid; 
+    subscriberId : guid
+}
+type hashTagMsg = {
+    ht : string; 
+    userId : guid
+}
+type mentionMsg = {
+    men : string; 
+    userId : guid
+}
+type retweetMsg = {
+    userId : guid; 
+    tweet : string
+}
+
+let configuration = 
+    ConfigurationFactory.ParseString(
+        @"akka {
+            actor {
+                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                debug : {
+                    receive : on
+                    autoreceive : on
+                    lifecycle : on
+                    event-stream : on
+                    unhandled : on
+                }
+            }
+            remote {
+                helios.tcp {
+                    port = 8123
+                    hostname = localhost
+                }
+            }
+        }")
+
+let system = ActorSystem.Create("Twitter", configuration)
+let echoClient = system.ActorSelection("akka.tcp://Twitter@localhost:5000/user/EchoServer")
+
+let mutable count = 0;
+
+type NodeMessage =
+    |Start of (int)
+    |Inp
+    |End of (int)
+
+let hts = [|"#ht1" ; "#ht2" ; "#ht3" ; "#ht4" ; "#ht5" ; "#ht6"|]
+
+let Node (listener : IActorRef) (x : int) (mailBox:Actor<_>) = 
+        let rec loop() = actor {
+            
+            let! message = mailBox.Receive()
+                    
+            match message with 
+                | Start(x) ->
+                        let mutable responses = "";
+                        let userId = ((new System.Random()).Next(numberOfUsers)) + 1
+                        // printfn "%d  --" userId
+                        let j = ((new System.Random()).Next(6)) 
+                        if (j = 0) then
+                            let tweetID = {guid.id = "user" + userId.ToString()}
+                            let tweet = "tweet" + userId.ToString() + " #game @user1"
+                            let task = echoClient <? {tweetMsg.userId = tweetID; tweetMsg.tweet = tweet}
+                            responses <- Async.RunSynchronously (task, 3000);
+                        else if (j = 1) then
+                            let registerMessageID = {guid.id = "user" + userId.ToString()}
+                            let registerMessagePassword = "password" + userId.ToString()
+                            let task = echoClient <? {registerMsg.code = 2; registerMsg.userId = registerMessageID; registerMsg.password = registerMessagePassword}
+                            responses <- Async.RunSynchronously (task, 1000);
+                        else if (j = 2) then
+                            let registerMessageID = {guid.id = "user" + userId.ToString()}
+                            let registerMessagePassword = "password" + userId.ToString()
+                            let task = echoClient <? {registerMsg.code = 1; registerMsg.userId = registerMessageID; registerMsg.password = registerMessagePassword}
+                            responses <- Async.RunSynchronously (task, 1000);
+                        else if (j = 3) then
+                            let l1 = ((new System.Random()).Next(6))
+                            let hashtagMessage = {guid.id = "user" + userId.ToString()}
+                            let task = echoClient <? {hashTagMsg.ht = hts.[l1]; hashTagMsg.userId = hashtagMessage}
+                            responses <- Async.RunSynchronously (task, 1000);
+                        else if (j = 4) then
+                            let mentionMessage = "@user" + userId.ToString()
+                            let mentionUserID = {guid.id = "user" + userId.ToString()}
+                            let task = echoClient <? {mentionMsg.men = mentionMessage; mentionMsg.userId = mentionUserID}
+                            responses <- Async.RunSynchronously (task, 1000);
+                        else if (j = 5) then
+                            let retweetUserID = {guid.id = "user2"}
+                            let retweetUserMessage = "Retweetedby:usery|author:userx = tweetx #game @user1"
+                            let task = echoClient <? {retweetMsg.userId = retweetUserID; retweetMsg.tweet = retweetUserMessage}
+                            responses <- Async.RunSynchronously (task, 1000);
+                        listener <! End(userId)
+
+                | _-> return! loop()
+            return! loop()
+        }
+        loop()
+
+let BossActor (mailbox:Actor<_>) =
+    let rec loop () = actor {
+        let! message = mailbox.Receive()
+        match message with 
+        | Inp ->
+            let node = Node mailbox.Context.Self (0) |> spawn system ("Node" + id.ToString())
+            for i in 1 .. numberOfUsers do
+                node <! Start(i)
+
+        | End(x) ->
+            count <- count + 1;
+            if (count = numberOfUsers) then
+                mailbox.Context.System.Terminate() |> ignore
+
+        | _ -> ()
+        return! loop()
+    }
+    loop()
+
+let mutable response = "";
+
+let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew();
+
+let mutable i = 1;
+while i < numberOfUsers do
+    // printfn "%d" i
+    let messageUserID = {guid.id = "user" + i.ToString()}
+    let messagePassword = "password" + i.ToString()
+    let task = echoClient <? {registerMsg.code = 0; registerMsg.userId = messageUserID; registerMsg.password = messagePassword}
+    response <- Async.RunSynchronously (task, 1000);
+    i <- i + 1;
+
+stopWatch.Stop();
+let timeToRegisterUsers = stopWatch.Elapsed.TotalMilliseconds
+
+stopWatch <- System.Diagnostics.Stopwatch.StartNew();
+
+let mutable step = 1;
+for i in 1..numberOfUsers do
+    for j in 1..step..numberOfUsers do
+        // printfn "%d == %d" i j
+        let mutable res = -1;
+        if ( i <> j ) then
+            let a =
+                if (j <> 0) then
+                    res <- j;
+                else
+                    res <- i / 2;
+                res
+            let subscribingUserID = {guid.id = "user" + a.ToString()}
+            let subscribingSubscriberID = {guid.id = "user" + i.ToString()}
+            let task = echoClient <? {subscribeMsg.userId = subscribingUserID; subscribeMsg.subscriberId = subscribingSubscriberID}
+            response <- Async.RunSynchronously (task, 1000);
+    step <- (i + 1) * (i + 1);
+
+// printfn "Done Subscribing"
+stopWatch.Stop();
+let timeToSubscribe = stopWatch.Elapsed.TotalMilliseconds
+
+stopWatch <- System.Diagnostics.Stopwatch.StartNew();
+
+let mutable k = 1;
+while k < numberOfUsers do 
+    let j = ((new System.Random()).Next(5))
+    let tweetMessageID = {guid.id = "user" + k.ToString()}
+    let tweetMessage = "tweet" + k.ToString() + "0" + " " + hts.[j] + " @user" + (k / 2).ToString()
+    let task = echoClient <? {tweetMsg.userId = tweetMessageID; tweetMsg.tweet =  tweetMessage}
+    response <- Async.RunSynchronously (task, 1000);
+    k <- k + 1;
+
+stopWatch.Stop();
+let timeToSendTweets = stopWatch.Elapsed.TotalMilliseconds
+
+stopWatch <- System.Diagnostics.Stopwatch.StartNew();
+
+let boss = spawn system "boss" BossActor
+boss <? Inp
+system.WhenTerminated.Wait()
+
+stopWatch.Stop();
+let timeToPerformOperations = stopWatch.Elapsed.TotalMilliseconds
+
+printfn "**********************************************************************" 
+printfn "The time taken to register %d users is %f" numberOfUsers timeToRegisterUsers
+printfn "The time taken to subscribe in Zipf distance for %d users is %f" numberOfUsers timeToSubscribe
+printfn "The time taken to send tweets for %d users is %f" numberOfUsers timeToSendTweets
+printfn "The time taken to perform random operations for %d users is %f" numberOfUsers timeToPerformOperations
+printfn "**********************************************************************"
+
+system.Terminate() |> ignore
---
+++ Server.fsx
@@ -0,0 +1,339 @@
+﻿#r "nuget: Akka" 
+#r "nuget: Akka.FSharp" 
+#r "nuget: Akka.Remote" 
+#r "nuget: Akka.TestKit" 
+
+open Akka.Actor
+open Akka.Configuration
+open Akka.FSharp
+
+let configuration = 
+    ConfigurationFactory.ParseString(
+        @"akka {
+            actor {
+                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                debug : {
+                    receive : on
+                    autoreceive : on
+                    lifecycle : on
+                    event-stream : on
+                    unhandled : on
+                }
+            }
+            remote {
+                helios.tcp {
+                    port = 5000
+                    hostname = localhost
+                }
+            }
+        }")
+
+[<Struct>]         
+type guid = {
+    id : string
+}
+type registerMsg = {
+    code : int; 
+    userId : guid; 
+    password : string
+}
+type tweetMsg = {
+    userId : guid; 
+    tweet : string
+}
+type subscribeMsg = {
+    userId : guid; 
+    subscriberId : guid
+}
+type hashTagMsg = {
+    ht : string; 
+    userId : guid
+}
+type mentionMsg = {
+    men : string; 
+    userId : guid
+}
+type retweetMsg = {
+    userId : guid; 
+    tweet : string
+}
+
+let mutable userIdSet: Set<string> = Set.empty;
+let mutable userId:  Map<string, string> = Map.empty;
+let mutable onlineUsers:  Map<string, bool> = Map.empty;
+let mutable hashTags: Map<string, list<string>> = Map.empty;
+let mutable mentionUsers: Map<string, list<string>> = Map.empty;
+let mutable pendingRequests: Map<string, list<string>> = Map.empty;
+let mutable followers: Map<string, list<string>> = Map.empty;
+let mutable followingMap: Map<string, list<string>> = Map.empty;
+
+let system = System.create "Twitter" configuration
+
+let registerUser (id: string) (password: string) =
+    let mutable found = "";
+    if (userIdSet.Contains id) then
+        found <- "user already exists";
+    else 
+        userIdSet <- userIdSet.Add(id);
+        userId <- userId.Add(id, password);
+        onlineUsers <- onlineUsers.Add(id, true);
+        mentionUsers <- mentionUsers.Add(id, []);
+        followingMap <- followingMap.Add(id, List.empty);
+        followers <- followers.Add(id, List.empty);
+        pendingRequests <- pendingRequests.Add(id, []);
+        found <- "registration is successful";
+    found
+
+let ActivateUser (userId: string) (newStatus: bool) =
+    onlineUsers <- onlineUsers.Add(userId, newStatus)
+
+let addUserAsFollowing (user: string) (following: string) =
+    let mutable followingUser = followingMap.Item user;
+    followingUser <- followingUser @ [ following ];
+    followingMap <- followingMap.Add(user, followingUser);
+
+let addFollowerAndLeader (userId: string) (leaderId: string) =
+    let mutable found = false;
+    if((userIdSet.Contains(userId)) && (userIdSet.Contains(leaderId))) then
+        addUserAsFollowing userId leaderId
+        addUserAsFollowing userId leaderId
+        found <- true;
+    found
+
+
+let retreiveFromList (arr: string list) (symbol: char) =
+    let mutable dataList : List<string> = List.Empty;
+    let myVar = symbol.ToString();
+    for i in arr do
+        if i.StartsWith myVar then
+            dataList <- List.append dataList [i];
+    dataList
+
+let getHashTags (arr: string list) =   
+    retreiveFromList arr '#'
+
+let getMentions (arr: string list) =
+    retreiveFromList arr '@'
+
+let saveHashTags (hashtagList: list<string>) (tweet: string) =
+    for hashtag in hashtagList do
+        if hashTags.ContainsKey hashtag then
+            let currentTweetList = hashTags.Item hashtag
+            let tweetList = currentTweetList @ [ tweet ]
+            hashTags <- hashTags.Add(hashtag, tweetList)
+        else
+            let tweetList = [ tweet ]
+            hashTags <- hashTags.Add(hashtag, tweetList)
+
+
+let pushFeedToUser (username: string) =
+    let pendingFeed = pendingRequests.Item username
+    let newEmptyFeed = [ "start" ]
+    pendingRequests <- pendingRequests.Add(username, newEmptyFeed)
+    pendingFeed
+
+type Message_reg = MessageReg of  int  * guid  * string * IActorRef
+type Message_tweet = MessageTweet of  guid * string * IActorRef
+type Message_retweet = MessageRetweet of  guid * string * IActorRef
+type Message_subscribe = MessageSubscribe of  guid * guid * IActorRef
+type Message_HT = MessageHT of  string * IActorRef
+type Message_Mention = MessageMention of  string * IActorRef
+
+let MentionWorker (mailbox: Actor<_>)=
+        let rec loop() =
+            actor {
+                let! message = mailbox.Receive()
+                match message with
+                | MessageMention(mentionedUser,sender) -> 
+                        if (mentionUsers.ContainsKey mentionedUser) then
+                            let mentionedList = mentionUsers.Item mentionedUser
+                            let mutable res = "";
+                            for men in mentionedList do
+                                res <- res + "***" + men;
+                            sender <! res
+                        else
+                            sender <! "no such mentions"
+                return! loop()
+            } 
+        loop()
+
+let HashTagWorker (mailbox: Actor<_>)=
+    let rec loop() =
+        actor {
+            let! message = mailbox.Receive()
+            match message with
+            | MessageHT(hashtag,sender) -> 
+                    if (hashTags.ContainsKey hashtag) then
+                        let hashtagTweets = hashTags.Item hashtag
+                        let mutable res = "";
+                        for ht in hashtagTweets do
+                            res <- res + "***" + ht;
+                        sender <! res
+                    else
+                        sender <! "no such hashtags"
+            return! loop()
+        } 
+    loop()
+
+let SubscribeWorker (mailbox: Actor<_>)=
+    let rec loop() =
+        actor {
+            let! message = mailbox.Receive()
+
+            match message with
+            | MessageSubscribe(user, peer, sender) -> 
+                    addFollowerAndLeader user.id peer.id |> ignore
+                    sender <! "success"
+            return! loop()
+        } 
+    loop()
+
+let ReweetsWorker (mailbox: Actor<_>)=
+    let rec loop() =
+        actor {
+            let! message = mailbox.Receive()
+
+            match message with
+            | MessageRetweet(user, tweet, sender) -> 
+                    let followersList = followers.Item user.id
+                    for follower in followersList do
+                        if(tweet.StartsWith "Retweetedby") then
+                            let arr = tweet.Split('|') |> Array.toList
+                            let currentToUserFeed = pendingRequests.Item follower
+                            let hybridTweet = "Retweetedby:" + user.id + "|" + arr.[1]
+                            let newToUserFeed = currentToUserFeed @ [ hybridTweet ]
+                            pendingRequests <- pendingRequests.Add(follower, newToUserFeed)
+                        else
+                            let currentToUserFeed = pendingRequests.Item follower
+                            let hybridTweet = "Retweetedby:" + user.id + "|" + tweet
+                            let newToUserFeed = currentToUserFeed @ [ hybridTweet ]
+                            pendingRequests <- pendingRequests.Add(follower, newToUserFeed)
+
+                    sender <! "success"
+            return! loop()
+        } 
+    loop()
+
+let TweetsWorker (mailbox: Actor<_>)=
+    let rec loop() =
+        actor {
+            let! message = mailbox.Receive()
+
+            match message with
+            | MessageTweet(user, tweet, sender) -> 
+                    let hashTags = getHashTags (tweet.Split(' ') |> Array.toList)
+                    saveHashTags hashTags tweet
+                    let mentions = getMentions (tweet.Split(' ') |> Array.toList)
+                    for mention in mentions do
+                        if mentionUsers.ContainsKey mention then
+                            let currentTweetList = mentionUsers.Item mention
+                            let tweetList = currentTweetList @ [ tweet ]
+                            mentionUsers <- mentionUsers.Add(mention, tweetList)
+                        else
+                            let tweetList = [ tweet ]
+                            mentionUsers <- mentionUsers.Add(mention, tweetList)
+                    let followersList = followers.Item user.id
+                    for follower in followersList do
+                        let currentToUserFeed = pendingRequests.Item follower
+                        let hybridTweet = "author:" + user.id + " = " + tweet
+                        let newItemInUserFeed = currentToUserFeed @ [ hybridTweet ]
+                        pendingRequests <- pendingRequests.Add(follower, newItemInUserFeed)
+
+                    sender <! "success"
+            return! loop()
+        } 
+    loop()
+
+let AuthenticationWorker (mailbox: Actor<_>)=
+    let rec loop() =
+        actor {
+            let! message = mailbox.Receive()
+
+            match message with
+            | MessageReg(code, username, pwd, sender) -> 
+                if(code = 0) then
+                    let res = registerUser username.id pwd
+                    sender <! res
+                else if(code = 1) then
+                    let mutable res = "login failed";
+                    if(userId.ContainsKey username.id) then
+                        let pword = userId.Item username.id
+                        if (pword = pwd) then
+                            ActivateUser username.id true
+                            res <- "";
+                            let pfeed = pushFeedToUser username.id
+                            for pf in pfeed do
+                                res <- res + "***" + pf;
+                        sender <! res
+                else
+                    ActivateUser username.id false
+                    sender <! "successfully logged out"
+            return! loop()
+        } 
+    loop()
+
+let AuthWorker = spawn system "AuthenticationWorker" AuthenticationWorker
+let TweetWorker = spawn system "TweetWorker" TweetsWorker
+let RetweetWorker = spawn system "RetweetWorker" ReweetsWorker
+let SubscriberWorker = spawn system "SubscriberWorker" SubscribeWorker
+let HashTagsWorker =  spawn system "HashTagsWorker" HashTagWorker
+let MentionsWorker =  spawn system "MentionsWorker" MentionWorker
+
+let isUserLoggedIn (userId: guid) =
+    let mutable found = false;
+    if(onlineUsers.ContainsKey userId.id) then
+        if(onlineUsers.Item userId.id) then
+            found <- true;
+    found
+
+let echoServer = 
+    spawn system "EchoServer"
+    <| fun mailbox ->
+        let rec loop() =
+            actor {
+                let! message = mailbox.Receive()
+                let sender = mailbox.Sender()
+                let userMessage = "user not logged in"
+                
+                match box message with
+                | :? registerMsg as msg-> 
+                    AuthWorker <! MessageReg(msg.code, msg.userId, msg.password, sender)
+
+                | :? tweetMsg as msg-> 
+                    if (isUserLoggedIn msg.userId) then
+                        TweetWorker <! MessageTweet(msg.userId, msg.tweet, sender)
+                    else
+                        sender <! userMessage
+
+                | :? retweetMsg as msg-> 
+                    if (isUserLoggedIn msg.userId) then
+                        RetweetWorker <! MessageRetweet(msg.userId, msg.tweet, sender)
+                    else
+                        sender <! userMessage
+
+                | :? subscribeMsg as msg-> 
+                    if (isUserLoggedIn msg.userId) then
+                        SubscriberWorker <! MessageSubscribe(msg.userId, msg.subscriberId, sender)
+                    else
+                        sender <! userMessage
+
+                | :? hashTagMsg as msg-> 
+                    if (isUserLoggedIn msg.userId) then
+                        HashTagsWorker <! MessageHT(msg.ht, sender)
+                    else
+                        sender <! userMessage
+
+                | :? mentionMsg as msg-> 
+                    if (isUserLoggedIn msg.userId) then
+                        MentionsWorker <! MessageMention(msg.men, sender)
+                    else
+                        sender <! userMessage
+
+                | _ ->  failwith "Unknown message"
+                
+                return! loop()
+            }
+        loop()
+
+System.Console.ReadLine() |> ignore
+system.Terminate() |> ignore
